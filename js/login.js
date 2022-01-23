8a939faf7654c29358bdb4001a16b559b413d2a3
reasley <reasley.com@gmail.com>
2022-01-24 04:58:49 +0900

    Chore: 로그인 기능 수정
    오류가 발생하던 문제를 해결함
    - null 이벤트

--- js/login.js
+++ js/login.js
@@ -1,24 +1,27 @@
-const loginForm   = document.querySelector("#loginForm")
-const loginInput  = document.querySelector("#loginInput")
+const loginForm = document.querySelector("#loginForm")
+const loginInput = document.querySelector("#loginInput")
 const loginButton = document.querySelector("#loginButton")
-const userNotice = document.querySelector('#userNotice')
+const greeting = document.querySelector("#userNotice")
 
-const savedLocalStorage = localStorage.getItem("userSubmit");
+const savedLocalStorage = localStorage.getItem("userSubmit")
 
 const time = new Date()
 const hour = time.getHours()
 
+const HIDDEN_KEY = "hidden"
+
 function handleLoginSubmit(event) {
     event.preventDefault()
     const userSubmit = loginInput.value
-    savedLocalStorage.setItem("userSubmit", userSubmit)
+    localStorage.setItem("userSubmit", userSubmit)
+    greeting.innerText = `Hello, ${userSubmit}!`
     hidden()
-    paintuserNotice(userSubmit)
+    showing()
 }
 
-function paintuserNotice(userSubmit) {
-    showTodoInput()
-    userNotice.innerText = `Hello, ${userSubmit}!`
+function showing() {
+    greeting.classList.remove('hidden')
+    todoInput.classList.remove('hidden')
 }
 
 function hidden() {
@@ -26,12 +29,9 @@ function hidden() {
     loginButton.classList.add('hidden')
 }
 
-function showTodoInput() {
-    todoInput.classList.remove('hidden');
-}
-if(savedLocalStorage === null) { loginForm.addEventListener('submit', handleLoginSubmit) }
-else {
-    loginInput.classList.add('hidden')
-    loginButton.classList.add('hidden')
-    paintuserNotice(savedLocalStorage)
+if(savedLocalStorage === null) {
+    loginForm.addEventListener("submit", handleLoginSubmit)
+} else {
+    hidden()
+    paintGreeting(savedLocalStorage)
 }
